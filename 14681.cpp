def1d90427e0e999c1d6badaed8eb9b98723baa4
zogak <zogak7@gmail.com>
2021-03-27 21:47:37 +0900

    no.14681 select quadrant

---
+++ 14681.cpp
@@ -0,0 +1,21 @@
+/*select quadrant*/
+#include <iostream>
+using namespace std;
+int x, y;
+int main() {
+	cin >> x;
+	cin >> y;
+	if (x > 0 && y > 0) {
+		cout << 1;
+	}
+	else if (x < 0 && y > 0) {
+		cout << 2;
+	}
+	else if (x < 0 && y < 0) {
+		cout << 3;
+	}
+	else {
+		cout << 4;
+	}
+	return 0;
+}
