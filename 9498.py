fa5406c6f4d643e3e65db41f1c311b007c3c807a
zogak <zogak7@gmail.com>
2021-04-10 19:19:24 +0900

    no.9498 test score

---
+++ 9498.py
@@ -0,0 +1,13 @@
+#test score
+score = int(input())
+
+if score>=90 and score<=100:
+    print("A")
+elif score>=80:
+    print("B")
+elif score>=70:
+    print("C")
+elif score>=60:
+    print("D")
+else:
+    print("F")
